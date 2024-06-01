315ecb4c6ee5b9f2f161efe1909d379c063d0f30
hendisantika <hendisantika@yahoo.co.id>
2024-06-02 06:50:09 +0700

    Create Spring Boot App 🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/nextjssample/SpringBootNextjsSampleApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.nextjssample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNextjsSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootNextjsSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/nextjssample/SpringBootNextjsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.nextjssample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNextjsSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
