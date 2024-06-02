7d13bd574c5afcc4d0be679e7945fc3aea34e0cb
hendisantika <hendisantika@yahoo.co.id>
2024-06-02 07:37:09 +0700

    Add CarSearchParametersUtil 🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/nextjssample/util/CarSearchParametersUtil.java
@@ -0,0 +1,14 @@
+package id.my.hendisantika.nextjssample.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-nextjs-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 02/06/24
+ * Time: 07.36
+ * To change this template use File | Settings | File Templates.
+ */
+public class CarSearchParametersUtil {
+}
