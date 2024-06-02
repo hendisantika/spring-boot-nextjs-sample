9693ce492123be4ba258c9819cae1096cf142a49
hendisantika <hendisantika@yahoo.co.id>
2024-06-02 07:39:45 +0700

    Add Range in CarSearchParametersUtil 🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/nextjssample/util/CarSearchParametersUtil.java
+++ src/main/java/id/my/hendisantika/nextjssample/util/CarSearchParametersUtil.java
@@ -80,4 +80,28 @@ public class CarSearchParametersUtil {
             int end = Integer.parseInt(parts[1]);
             return new Range(start, end);
         }
+    }
+
+    public static class Range {
+
+        private final int start;
+        private final int end;
+
+        public Range(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        public int getStart() {
+            return start;
+        }
+
+        public int getEnd() {
+            return end;
+        }
+    }
 }
+
+
+
+
