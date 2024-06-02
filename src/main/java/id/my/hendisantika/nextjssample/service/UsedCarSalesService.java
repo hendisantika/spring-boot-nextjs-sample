a785b33454f102fcf809663c2fd653a18e265771
hendisantika <hendisantika@yahoo.co.id>
2024-06-02 07:36:39 +0700

    Add findByParameter in UsedCarSalesService 🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/nextjssample/service/UsedCarSalesService.java
+++ src/main/java/id/my/hendisantika/nextjssample/service/UsedCarSalesService.java
@@ -1,8 +1,12 @@
 package id.my.hendisantika.nextjssample.service;
 
+import id.my.hendisantika.nextjssample.entity.UsedCarSales;
+import id.my.hendisantika.nextjssample.model.CarSearchParameters;
 import id.my.hendisantika.nextjssample.repository.UsedCarSalesRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +23,8 @@ import org.springframework.stereotype.Service;
 public class UsedCarSalesService {
 
     private final UsedCarSalesRepository usedCarSalesRepository;
+
+    public List<UsedCarSales> findByParameter(CarSearchParameters carSearchParameters) {
+        return usedCarSalesRepository.findByParameter(carSearchParameters);
+    }
 }
