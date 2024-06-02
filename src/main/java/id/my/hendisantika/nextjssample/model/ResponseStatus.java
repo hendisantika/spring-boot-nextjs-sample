283f6033a8f83b71e39856e5d7da070b60593f5a
hendisantika <hendisantika@yahoo.co.id>
2024-06-02 07:11:30 +0700

    Add ResponseStatus Model 🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/nextjssample/model/ResponseStatus.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.nextjssample.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-nextjs-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 02/06/24
+ * Time: 07.11
+ * To change this template use File | Settings | File Templates.
+ */
+public class ResponseStatus {
+    public static final String SUCCESS = "success";
+    public static final String FAIL = "fail";
+    public static final String ERROR = "error";
+    public static final String NOT_FOUND = "not_found";
+    public static final String UNAUTHORIZED = "unauthorized";
+    public static final String INVALID_PARAMETERS = "invalid_parameters";
+    public static final String DATABASE_ERROR = "database_error";
+    public static final String SERVER_ERROR = "server_error";
+}
