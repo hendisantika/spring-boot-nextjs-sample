ff4bcd1856aff7e0adfcdc80130c1e5edd9e270e
hendisantika <hendisantika@yahoo.co.id>
2024-12-19 09:25:58 +0700

    feat: Modify Integration Test🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/nextjssample/SpringBootNextjsSampleApplicationTests.java
+++ src/test/java/id/my/hendisantika/nextjssample/SpringBootNextjsSampleApplicationTests.java
@@ -1,13 +1,34 @@
 package id.my.hendisantika.nextjssample;
 
-import org.junit.jupiter.api.Test;
+import id.my.hendisantika.nextjssample.repository.UsedCarSalesRepository;
+import id.my.hendisantika.nextjssample.repository.UsersRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@Testcontainers
+@SpringBootTest(
+        properties = {
+                "management.endpoint.health.show-details=always",
+                "spring.datasource.url=jdbc:tc:mysql:9.1.0:///carsDB"
+        },
+        webEnvironment = RANDOM_PORT
+)
 class SpringBootNextjsSampleApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    private UsedCarSalesRepository usedCarSalesRepository;
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @BeforeEach
+    void deleteAll() {
+        usedCarSalesRepository.deleteAll();
+        usersRepository.deleteAll();
     }
 
 }
